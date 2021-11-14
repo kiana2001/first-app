bbce91554d48651925b84b6ba45d0004cc8e75d4
kiana2001 <kianakermani1381@gmail.com>
2021-11-14 12:29:40 +0330

    سخپث زخپپهف

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,16 +1,16 @@
 import React from "react";
  import styles from "./Navbar.module.css";
 import photo1 from "../images/photo1.png";
-
+import {Link} from "react-router-dom";
 
 const Navbar = () =>{
     return (
     <header className = {styles.header}>
         <div className = {styles.listContainer}>
              <ul className= {styles.list}>
-                <li>home page</li>
-             <li>about us</li>
-             <li>home page</li>
+                <li><Link to ="/">home page</Link></li>
+             <li><Link to = "/products">Products</Link></li>
+             <li><Link to = "/aboutus">about us</Link> </li>
         </ul>
 </div>
 <div className={styles.logo}>
